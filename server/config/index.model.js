1396953b62ef09f498ce7554bfece39669ca7d0c
Your Name <your.email@example.com>
2026-02-11 16:32:57 +0200

    mysql2

--- server/config/index.model.js
+++ server/config/index.model.js
@@ -4,14 +4,24 @@ import initLogo from '../models/logo.model.js';
 import initCardsModel from '../models/cards.model.js';
 import initTestimony from '../models/Testimony.model.js';
 import initLocationModel from '../models/address-image.js';
-import initReservetion  from '../models/reserve-table.model.js';
+import initReservetion from '../models/reserve-table.model.js';
 import initUserModel from '../models/users.model.js';
 
 import dotenv from 'dotenv';
 dotenv.config();
-const connection = new Sequelize(process.env.DATABASE_CONNECTION, { dialect: 'mysql', logging: false });
+const connection = new Sequelize(process.env.DATABASE_CONNECTION, {
+  dialect: 'mysql',
+  logging: false,
+  dialectModule: 'mysql2',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 connection.authenticate()
-  .then(() => console.log('connected to database server!'))
+  .then(() => console.error('connected to database server!'))
   .catch(err => console.log(err.message));
 
 const Logo = initLogo(connection);
@@ -23,4 +33,4 @@ const User = initUserModel(connection)
 // connection.sync({alter:true})
 
 export default connection;
-export { connection , Cards, Logo, Testimonies, Location, reservetion, User, Op}
+export { connection, Cards, Logo, Testimonies, Location, reservetion, User, Op }
